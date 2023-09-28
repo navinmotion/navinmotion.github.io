10c2891900154c5aa42c9fabe6b96818f3d91ba2
Ankit Kumar <120427738+blackhat-coder21@users.noreply.github.com>
2023-09-28 18:06:12 +0530

    Uploaded Resources
    Uploading assests folder

---
+++ assets/js/imageGrid.js
@@ -0,0 +1,206 @@
+const posts = [];
+const images = [
+  'https://lh3.googleusercontent.com/drive-viewer/AITFw-xht1mxOqeZYsfsBHiSKIUEXOXysESmA7hCTRKduiTANo8FASY1f8dctZU1dmVGW6yK15Fj2_BFYNwYF4YZ11Y0-2ob4g=s1600',
+  'https://lh3.googleusercontent.com/drive-viewer/AITFw-y4c6Jzmyrdne1OLM4FVcCCfn1idFMVeYtxH-yOnrwPJbdju78EqmY9kYI3WpS0Bs4bT8wqYJNhxr1f15mODygjUVnSVg=s2560',
+  'https://lh3.googleusercontent.com/drive-viewer/AITFw-yoxKAAojB_sgXwhhQKnl8qI0dlI-OIO6huLyliwegACdGNOQOak68xCrLhSOJ_tyE4u9H22RTjq0U9PP3H9d9C7bQkSQ=s1600',
+  'https://lh3.googleusercontent.com/drive-viewer/AITFw-y3e4xb4QWRsOnhVry_zJbIYxIROrsaPDSWTSYRrCVQNpI8I6xewDGiSwloj1R27jrVsQ2e4PwOIrusCRVs0EbS11CU5w=s2560',
+  'https://lh3.googleusercontent.com/drive-viewer/AITFw-ytSCvQt9FK1SWkPA9s8hCoDn1vASKQNxZ0x-ETlp39KckaUOWVNIivkcJmAEW1U0HJQZ_Knq5MOGT6ugVDLW9tqqcGww=s2560',
+  'https://lh3.googleusercontent.com/drive-viewer/AITFw-zIOGP2XcvpPY84upekVFnWvmDft0lQDUsgVURFEXoBBXNVbNFbp3n96C0xHh4IIjzMf4GyHP4YpZYKTiAAivEgNS1E2A=s1600',
+  'https://lh3.googleusercontent.com/drive-viewer/AITFw-xAoGFu-HPtPV0wueygjvo6t8NAJfJ52GzmqDu1YWooCEYlFmuc4DmW1yh4-CSyKgOQbO7ro_EeMXoV3YPp2uw7bkCJBA=s1600',
+  'https://lh3.googleusercontent.com/drive-viewer/AITFw-wRhXog1FYxeVd2mXcNaOZ1lJ3mPZWVCb4iLr-W8BlzSda5UWCwLMJ7U6JJ5CGmRcnUDwe-iXsODtgTsdSAv6CXzKRz6g=s1600',
+  'https://lh3.googleusercontent.com/drive-viewer/AITFw-yN1iyC-qSEUJLifPa9smQ4br0nCbKBCor52TKpJKKdis6bhh-GqjMsTVwOgDCFqPnUg0heTH8lQnZx39Uv6nCRPzHfzQ=s2560',
+  'https://lh3.googleusercontent.com/drive-viewer/AITFw-xwnwf5PHbqk12EAyMBFfb-P7MyeF-OrQM9EqLGJcIJmmQlTBxYN2q16Ds66WJ7mKtQINa0voCH46Rr0fFyEgQ_D2mq8g=s1600',
+  'https://lh3.googleusercontent.com/drive-viewer/AITFw-zXSwY-CJq-Fw01CQy_KKSi8ITiZnFbgQwI4qvwGaK9Evkehvw_PThwMDh1jU6Sy5dfqgrB3zjmiMB6RxM1M183T8P1lw=s1600',
+];
+
+
+const postContents = [
+  // pop-up info list for image 1
+  {
+    parameter:'TXT2IMG + HI-RES + CONTROLNET',
+    class: 'parameter',
+    heading: ['Prompt', 'Negative Prompt', 'Sampler', 'Model', 'CFG scale', 'Strength', 'Steps', 'Clip skip'
+      ],
+    content: [
+        'a beautiful Indian woman wearing saree, looking back, going to temple, UHD, highly detailed,sharp, intricate, professional, masterpiece, cinematic composition, art by Raja Ravi Varma',
+        '&lt;baddream&gt;',
+        'DMP++ 2M Karras',
+        'dreamshaper_v6.31.ckpt',
+        '7.5', '1.0', '30', '2'
+      ],
+    class: 'popup-content',
+  },
+  
+  // pop-up info list for image 2
+  {
+    parameter:'TXT2IMG',
+    class: 'parameter',
+    heading: ['Prompt', 'Negative Prompt', 'Sampler', 'Model', 'CFG scale', 'Strength', 'Steps', 'Clip skip'
+              ],
+    content: [
+            'establishing shot of small modern osaka house, pitched roof, trending on pixiv, beautiful gouache painting by hayao miyazaki, by studio ghibli, by atey ghailan, by james jean, trending on artstation, unreal engine 5, cinematic lighting, vivid colours',
+            '&lt;bad_prompt&gt;',
+            'DMP++ 2M Karras',
+            'sd_v1.5_f16.ckpt',
+            '10', '1.0', '30', '1'
+          ],
+    class: 'popup-content',
+  },
+  
+  // pop-up info list for image 3
+  {
+    parameter:'TXT2IMG + HI-RES',
+    class: 'parameter',
+    heading: ['Prompt', 'Negative Prompt', 'Sampler', 'Model', 'CFG scale', 'Strength', 'Steps', 'Clip skip'
+              ],
+    content: [
+          'fashion photography portrait of indian girl with blue hair, in lush jungle with flowers, 3d render, cgi, symetrical, octane render, 35mm, bokeh, 9:16, (intricate details:1.12), hdr, (intricate details, hyperdetailed:1.15), (natural skin texture, hyperrealism, soft light, sharp:1.2), detailed, sunlight passing through foliage, india',
+          '&lt;baddream&gt;',
+          'DPM++ SDE Karras',
+          'dreamshaper_v6.31.ckpt',
+          '9.0', '1.0', '30', '2'
+        ],
+    class: 'popup-content',
+  },
+  
+  // pop-up info list for image 4
+  {
+    parameter:'TXT2IMG + HI-RES',
+    class: 'parameter',
+    heading: ['Prompt', 'Negative Prompt', 'Sampler', 'Model', 'CFG scale', 'Strength', 'Steps', 'Clip skip'
+              ],
+    content: [
+          'A Ghibli style Indian woman, hot, seductive, erotic,athletic body, subtle smile, traditional rural home in background, art by Raja Ravi Varma',
+          '&lt;baddream&gt;',
+          'DPM++ SDE Karras',
+          'dreamshaper_v6.31.ckpt',
+          '9.0', '1.0', '30', '2'
+        ],
+    class: 'popup-content',
+  },
+  
+  // pop-up info list for image 5
+  {
+    parameter:'TXT2IMG',
+    class: 'parameter',
+    heading: ['Prompt', 'Negative Prompt', 'Sampler', 'Model', 'CFG scale', 'Strength', 'Steps', 'Clip skip'
+              ],
+    content: [
+          'establishing shot of small modern osaka house, pitched roof, trending on pixiv, beautiful gouache painting by hayao miyazaki, by studio ghibli, by atey ghailan, by james jean, trending on artstation, unreal engine 5, cinematic lighting',
+          'ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, bad composition, mutation, mutated, extra limbs, extra legs, extra arms, disfigured, deformed, cross-eye, body out of frame, blurry, bad art, bad anatomy, blurred, text, watermark, grainy',
+          'DPM++ 2M Karras',
+          'inkpunk_v2_f16.ckpt',
+          '11', '1.0', '30', '1'
+        ],
+    class: 'popup-content',
+  },
+  
+  // pop-up info list for image 6
+  {
+    parameter:'TXT2IMG + HI-RES + CONTROLNET',
+    class: 'parameter',
+    heading: ['Prompt', 'Negative Prompt', 'Sampler', 'Model', 'CFG scale', 'Strength', 'Steps', 'Clip skip'
+              ],
+    content: [
+          'a side angle view of an Indian sadhu, looking to sunrise, doing namaskar in a holy river, UHD, highly detailed, sharp, intricate, concept art, professional, masterpiece, unreal engine, vivid colours, bokeh, cinematic composition, by Raja Ravi Varma',
+          '&lt;baddream&gt;',
+          'DPM++ 2M Karras',
+          'dreamshaper_v6.31.ckpt',
+          '7.5', '1.0', '25', '1'
+        ],
+    class: 'popup-content',
+  },
+  
+  // pop-up info list for image 7
+  {
+    parameter:'TXT2IMG + HI-RES',
+    class: 'parameter',
+    heading: ['Prompt', 'Negative Prompt', 'Sampler', 'Model', 'CFG scale', 'Strength', 'Steps', 'Clip skip'
+              ],
+    content: [
+          'ghibli style fashion photography portrait of indian girl with blue hair, in lush jungle with flowers, 3d render, cgi, symetrical, octane render, 35mm, bokeh, 9:16, (intricate details:1.12), hdr, (intricate details, hyperdetailed:1.15), (natural skin texture, hyperrealism, soft light, sharp:1.2), detailed, sunlight passing through foliage, india',
+          '&lt;baddream&gt;',
+          'DPM++ 2M Karras',
+          'dreamshaper_v6.31.ckpt',
+          '9.0', '1.0', '25', '2'
+        ],
+    class: 'popup-content',
+  },
+  
+  // pop-up info list for image 8
+  {
+    parameter:'TXT2IMG',
+    class: 'parameter',
+    heading: ['Prompt', 'Negative Prompt', 'Sampler', 'Model', 'CFG scale', 'Strength', 'Steps', 'Clip skip'
+              ],
+    content: [
+          'nvinkpunk an epic majestic Indian god shiva, masterpiece, cinematic composition, dramatic scene, beautiful, aesthetic, lighting, concept art, sharp, highly detailed, 8k, trending',
+          'ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, bad composition, mutated, extra limbs, extra legs, extra arms, disfigured, deformed, cross-eye, body out of frame, blurry, bad art, bad anatomy, text, watermark, grainy',
+          'DPM++ 2M Karras',
+          'inkpunk_v2_f16.ckpt',
+          '7.5', '1.0', '25', '1'
+        ],
+    class: 'popup-content',
+  },
+  
+  // pop-up info list for image 9
+  {
+    parameter:'TXT2IMG',
+    class: 'parameter',
+    heading: ['Prompt', 'Negative Prompt', 'Sampler', 'Model', 'CFG scale', 'Strength', 'Steps', 'Clip skip'
+              ],
+    content: [
+          'professional oil painting of establishing shot of canal surrounded by verdant blue modern curved rustic greek tiled buildings, professional majestic oil painting by ed blinkey, atey ghailan, studio ghibli, by jeremy mann, greg manchess, antonio moro, trending on artstation, trending on cgsociety, volumetric lighting, dramatic lighting, dawn, water, canoes, refraction',
+          'ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, bad composition, mutated, extra limbs, extra legs, extra arms, disfigured, deformed, cross-eye, blurry, bad art, bad anatomy, grainy, text, watermarks, logos',
+          'DPM++ 2M Karras',
+          'sd_v1.5_f16.ckpt',
+          '7.5', '1.0', '25', '1'
+        ],
+    class: 'popup-content',
+  },
+  
+  // pop-up info list for image 10
+  {
+    parameter:'TXT2IMG',
+    class: 'parameter',
+    heading: ['Prompt', 'Negative Prompt', 'Sampler', 'Model', 'CFG scale', 'Strength', 'Steps', 'Clip skip'
+              ],
+    content: [
+          'establishing shot of small modern osaka house, pitched roof, trending on pixiv, beautiful gouache painting by hayao miyazaki, by studio ghibli, by atey ghailan, by james jean, trending on artstation, unreal engine 5, cinematic lighting',
+          'ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, bad composition, mutation, mutated, extra limbs, extra legs, extra arms, disfigured, deformed, cross-eye, body out of frame, blurry, bad art, bad anatomy, blurred, text, watermark, grainy',
+          'DPM++ 2M Karras',
+          'sd_v1.5_f16.ckpt',
+          '10', '1.0', '30', '2'
+        ],
+    class: 'popup-content',
+  },
+  
+  // pop-up info list for image 11
+  {
+    parameter:'TXT2IMG + HI-RES + CONTROLNET',
+    class: 'parameter',
+    heading: ['Prompt', 'Negative Prompt', 'Sampler', 'Model', 'CFG scale', 'Strength', 'Steps', 'Clip skip'
+              ],
+    content: [
+          'ghibli style a wide shot of Indian boy wearing dhoti kurta, amazed face expression, (Indian village in background:1.2), Cannon 35mm camera, UHD, highly detailed, sharp, intricate, concept art, professional, masterpiece, vivid colours, cinematic composition, by Raja Ravi Varma',
+          'ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, bad composition, mutated, extra limbs, extra legs, extra arms, disfigured, deformed, cross-eye, body out of frame, blurry, bad art, bad anatomy, text, watermark, grainy',
+          'DPM++ 2M Karras',
+          'ghibli_v1_f16.ckpt',
+          '7.5', '1.0', '30', '1'
+        ],
+    class: 'popup-content',
+  },
+  
+];
+
+for (let i = 0; i < images.length; i++) {
+  let item = {
+    id: i + 1, // Adding 1 to start IDs from 1
+    image: images[i],
+    parameter: postContents[i].parameter,
+    heading: postContents[i].heading, // Access heading from the object
+    content: postContents[i].content, // Access content from the object
+    class: postContents[i].class || '', // Access the CSS class from the object
+  };
+  posts.push(item);
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
-(function() {
+(function () {
   "use strict";
 
   /**
@@ -44,7 +44,7 @@
    * burgerMenu
    */
   const burgerMenu = select('.burger')
-  on('click', '.burger', function(e) {
+  on('click', '.burger', function (e) {
     burgerMenu.classList.toggle('active');
   })
 
@@ -60,9 +60,9 @@
 
       let portfolioFilters = select('#filters a', true);
 
-      on('click', '#filters a', function(e) {
+      on('click', '#filters a', function (e) {
         e.preventDefault();
-        portfolioFilters.forEach(function(el) {
+        portfolioFilters.forEach(function (el) {
           el.classList.remove('active');
         });
         this.classList.add('active');
@@ -70,7 +70,7 @@
         portfolioIsotope.arrange({
           filter: this.getAttribute('data-filter')
         });
-        portfolioIsotope.on('arrangeComplete', function() {
+        portfolioIsotope.on('arrangeComplete', function () {
           AOS.refresh()
         });
       }, true);
@@ -109,3 +109,177 @@
   });
 
 })()
+
+
+// Committed Changes on 28-08-2023
+
+function openPage(pageName, elmnt, color) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+
+  // Show the specific tab content
+  document.getElementById(pageName).style.display = "block";
+
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+
+
+function copyTextAndShowPopup(copyButtonId, copyPopupId, preClassName) {
+  const copyButton = document.getElementById(copyButtonId);
+  const preTag = document.querySelector(`.${preClassName}`);
+  const copyPopup = document.getElementById(copyPopupId);
+
+  copyButton.addEventListener('click', () => {
+    const tempTextArea = document.createElement('textarea');
+    tempTextArea.value = preTag.textContent;
+    document.body.appendChild(tempTextArea);
+    tempTextArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempTextArea);
+
+    // Show the corresponding popup
+    copyPopup.style.display = 'block';
+
+    // Hide the popup after a delay (e.g., 2 seconds)
+    setTimeout(() => {
+      copyPopup.style.display = 'none';
+    }, 2000); // 2 seconds
+  });
+}
+
+// Initialize copy functionality for the first set of elements
+copyTextAndShowPopup('copyButton1', 'copyPopup1', 'prompt1');
+
+// Initialize copy functionality for the second set of elements
+copyTextAndShowPopup('copyButton2', 'copyPopup2', 'prompt2');
+
+
+
+
+const iconContainer = document.querySelector('.icon-container');
+const popoverContent = document.querySelector('.popover-content');
+
+iconContainer.addEventListener('click', (e) => {
+  e.stopPropagation(); // Prevent clicks inside the icon container from closing the popover
+  const iconRect = iconContainer.getBoundingClientRect();
+  popoverContent.style.top = iconRect.bottom + 'px'; // Position below the icon
+  popoverContent.style.left = iconRect.left + 'px'; // Align with the left edge of the icon
+  popoverContent.style.display = popoverContent.style.display === 'block' ? 'none' : 'block';
+});
+
+document.addEventListener('click', (e) => {
+  if (!iconContainer.contains(e.target) && e.target !== popoverContent) {
+    popoverContent.style.display = 'none';
+  }
+});
+
+
+
+
+
+function copyText(contentId, copyImageClass, tickImageClass) {
+  const textToCopy = document.getElementById(contentId);
+
+  // Create a textarea element to temporarily hold the text
+  const textarea = document.createElement("textarea");
+  textarea.value = textToCopy.textContent;
+  document.body.appendChild(textarea);
+
+  // Select the text in the textarea and copy it to the clipboard
+  textarea.select();
+  document.execCommand("copy");
+
+  // Remove the textarea element
+  document.body.removeChild(textarea);
+
+  // Hide the copy button image and show the tick image
+  const copyImage = document.querySelector(`.${copyImageClass}`);
+  copyImage.style.display = "none";
+
+  // Find the corresponding tick image and show it
+  const tickImage = document.querySelector(`.${tickImageClass}`);
+  tickImage.style.display = "inline";
+
+  // Reset the tick image after a few seconds
+  setTimeout(() => {
+    copyImage.style.display = "inline";
+    tickImage.style.display = "none";
+  }, 2000); // Reset after 2 seconds
+}
+
+
+
+function adjustPromptBoxSize() {
+  const promptBox1 = document.getElementById('prompt-box1');
+  const content1 = document.getElementById('content1');
+  const promptBox2 = document.getElementById('prompt-box2');
+  const content2 = document.getElementById('content2');
+
+  const minHeight = 150;
+
+  // Get the height of the content for both containers
+  const contentHeight1 = content1.offsetHeight;
+  const contentHeight2 = content2.offsetHeight;
+
+
+  promptBox1.style.maxHeight = contentHeight1 < minHeight ? minHeight + 'px' : '300px';
+  promptBox2.style.maxHeight = contentHeight2 < minHeight ? minHeight + 'px' : '300px';
+  // Adjust the prompt box height based on the content height
+}
+
+// Call the adjustPromptBoxSize function when the page loads
+window.addEventListener('load', adjustPromptBoxSize);
+
+// Call the adjustPromptBoxSize function whenever the content changes
+// For example, you can call it after copying the text in your specific use case
+function copyText(contentId, copyImageClass, tickImageClass) {
+  const textToCopy = document.getElementById(contentId);
+
+  // Create a textarea element to temporarily hold the text
+  const textarea = document.createElement("textarea");
+  textarea.value = textToCopy.textContent;
+  document.body.appendChild(textarea);
+
+  // Select the text in the textarea and copy it to the clipboard
+  textarea.select();
+  document.execCommand("copy");
+
+  // Remove the textarea element
+  document.body.removeChild(textarea);
+
+  // Hide the copy button image and show the tick image
+  const copyImage = document.querySelector(`.${copyImageClass}`);
+  copyImage.style.display = "none";
+
+  // Find the corresponding tick image and show it
+  const tickImage = document.querySelector(`.${tickImageClass}`);
+  tickImage.style.display = "inline";
+
+  // Reset the tick image after a few seconds
+  setTimeout(() => {
+    copyImage.style.display = "inline";
+    tickImage.style.display = "none";
+  }, 2000); // Reset after 2 seconds
+
+  // After copying, adjust the prompt box size
+  adjustPromptBoxSize();
+}
+
+
+
+
