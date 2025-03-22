c66d7de889b7363d5a7bad3d8424d7d737b6e1e3
blackhat-coder21 <ankit.21.kumar02@gmail.com>
2025-03-22 13:11:13 +0530

    About Updated

---
+++ api/contact.js
@@ -0,0 +1,42 @@
+const nodemailer = require("nodemailer");
+
+export default async function handler(req, res) {
+    if (req.method !== "POST") {
+        return res.status(405).json({ message: "Method Not Allowed" });
+    }
+
+    const { name, email, subject, message } = req.body;
+
+    if (!name || !email || !subject || !message) {
+        return res.status(400).json({ message: "Please fill out all fields correctly." });
+    }
+
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: "navinkrworks@gmail.com",
+            pass: "irdixikwflbdplho"
+        }
+    });
+
+    const mailOptions = {
+        from: email,
+        to: "navinkrworks@gmail.com",
+        subject: "🚀 New Project Inquiry from Website",
+        text: `
+            📛 Name: ${name}
+            📧 Email: ${email}
+            📞 Subject: ${subject}
+            📝 Message: ${message}
+        `
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        return res.status(200).json({ message: "Email sent successfully!" });
+    } 
+    catch (error) {
+        console.error("Error sending email:", error);
+        return res.status(500).json({ message: "Failed to send email. Please try again later." });
+    }
+}
